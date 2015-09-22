d57822da773dd739f56360e8ccb79bee0085bff4
unknown <scnymc@scnymc.apac.sas.com>
2015-09-22 10:05:07 +0800

    init HelloSAS

---
+++ src/main/java/com/sas/hellosas/HelloBTG.java
@@ -0,0 +1,10 @@
+package com.sas.hellosas;
+
+public class HelloBTG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Mark Chen!");
+	}
+
+}
